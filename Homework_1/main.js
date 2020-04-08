b1988c6bc26a4c7275fb9661b16c7d72133672ef
Semen_K <idsemen1992@ya.ru>
2020-04-08 22:18:03 +0300

    first commit

---
+++ Homework_1/main.js
@@ -0,0 +1,11 @@
+//
+
+var body = document.getElementsByTagName('body');
+
+function changeColor(){
+    body[0].style.backgroundColor = 'red';
+}
+
+function changeBody(){
+    body[0].innerHTML = 'sdsd';
+}
