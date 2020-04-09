e61ddf58cf78de1d2150e35354201f810c58f5e3
Semen_K <idsemen1992@ya.ru>
2020-04-09 23:26:04 +0300

    second commit

--- Homework_1/main.js
+++ Homework_1/main.js
@@ -1,4 +1,4 @@
-//
+//Первое задание
 
 var body = document.getElementsByTagName('body');
 
@@ -7,5 +7,32 @@ function changeColor(){
 }
 
 function changeBody(){
-    body[0].innerHTML = 'sdsd';
+    body[0].innerHTML = '<div class="flex-wrapper"><div class="flex-item"></div><div class="flex-item"></div><div class="flex-item"></div></div>';
 }
+
+//Второе задание
+var term = {
+	belarus: 22,
+	spain: 18,
+	germany: 12,
+	poland: 11,
+	russia: 12,
+};
+
+//Среднее значение температуры
+// function summaryTerm() {
+// 	var sum = 0;
+// 	for (var temperature of Object.values(term)) {
+// 	sum += temperature;
+// 	}
+// 	return midlleTerm = sum / Object.keys(term).length;
+// }
+
+// console.log(summaryTerm(term));
+
+function maxTemp(){
+   for (var prop in term) {
+    console.log(term[prop]);
+   }
+}
+console.log(maxTemp(term));
