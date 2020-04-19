527020aa4ece6aa2a5ba73e593fed7bd1cc007e4
ss_kurochkin <idsemen1992@ya.ru>
2020-04-19 19:38:53 +0300

    hw_3-done

---
+++ Homework_3/script.js
@@ -0,0 +1,28 @@
+function Book(name, author, edition, editYear, examples, id, pages){
+    this.name = name;
+    this.author = author;
+    this.edition = edition;
+    this.editYear = editYear;
+    this.examples = examples;
+    this.id = id;
+    this.pages = pages;
+    this.getPages = function(){
+        return this.pages;
+    };
+}
+
+function Journal(edition, editYear, editNumber, id, pages){
+    this.edition = edition;
+    this.editYear = editYear;
+    this.editNumber = editNumber;
+    this.id = id;
+    if(pages <= 32){
+        this.pages = pages;
+    }
+    this.getPages = function(){
+        return this.pages;
+    };
+    this.getYear = function(){
+        return this.editYear;
+    };
+}
