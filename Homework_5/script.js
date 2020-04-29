deb41651862142556a05d309fbe48fcc86ad27b0
ss_kurochkin <idsemen1992@ya.ru>
2020-04-29 22:57:00 +0300

    commit2: add start/stop

--- Homework_5/script.js
+++ Homework_5/script.js
@@ -4,63 +4,61 @@ var box = document.getElementsByClassName('container');
 var minutes = 0;
 var seconds = 0;
 var milisec = 0;
-console.log(start);
-document.timer.display.value = 'Press start';
+var isGoing = true;
+document.timer.display.value = '0.0.0';
 
 
 
 start.addEventListener('click', function(event){
     if (start.textContent == 'Start'){
-        start.textContent = 'Stop';
+        start.textContent = 'Stop';        
+        isGoing = true;
         event.preventDefault();
     } else {
         start.textContent = 'Start';
     }
 });
 
-function goTime(){
-    var minutes = 0;
-    var seconds = 0;
-    var milisec = 0;
-    setTimeout(function go(){
-        if (milisec >= 9) {
-            milisec = 0;
-            seconds += 1;
+function goTime(){     
+    if (isGoing == true){
+            if (milisec > 8) {
+                milisec = 0;
+                seconds += 1;           
          } else if(seconds >= 59) {
              seconds = 0;
              minutes += 1;
          } else {
              milisec += 1;}
-     
-         document.timer.display.value =minutes + "." + seconds + "." + milisec;
-         setTimeout(go, 100);
-    }, 100);
+        }else{
+            
+        }       
+        document.timer.display.value = minutes + "." + seconds + "." + milisec;
 }
 
 start.addEventListener('click', function(event){
     if (start.textContent == 'Stop'){
-        goTime();
-    event.preventDefault();
+        IDint = setInterval(goTime, 100);
+        event.preventDefault();
+    }
+    if (start.textContent == 'Start') {
+        isGoing = false;
+        clearInterval(IDint);
+        event.preventDefault();
     }
 });
 
-// function writeTime(){
-//     var timeNow = document.timer.value;
-//     box[0].innerHTML = timeNow;
 
-// }
-
-// reset.addEventListener('click', writeTime());
-
-
-// start.addEventListener('click', changeButt());
-// function outputNumbers(one, two){
-//     var current = one;
-//     setTimeout( function go(){
-//         console.log(current)
-//         if(current !== two)
-//             setTimeout(go, 1000);
-//             current++;
-//         }, 1000);
-        
-// }
+function writeTime(){
+    var timeNow = document.createElement('p');
+    timeNow.innerHTML = document.timer.display.value;
+    box[0].appendChild(timeNow);
+}
+reset.addEventListener('click', function(){
+    writeTime();
+   
+    minutes = 0;
+    seconds = 0;
+    milisec = 0;
+    isGoing = false;
+    document.timer.display.value = '0.0.0';
+});
